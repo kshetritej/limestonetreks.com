53d878e8ef080723cef3020074eed8312a044640
kshetritej <tejghartikshetri@gmail.com>
2026-02-22 12:25:10 +0545

    add index and nofollow

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,15 @@ import "./globals.css";
 import Navbar from "@/components/common/navbar";
 import Footer from "@/components/common/footer";
 import { mulish } from "@/lib/font";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Limestone Treks",
   description: "Created by Growfore Solution",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
@@ -17,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${mulish.className} sans-serif antialiased`}
-      >
+      <body className={`${mulish.className} sans-serif antialiased`}>
         <Navbar />
         {children}
         <Footer />
