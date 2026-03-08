576382302e641177ea99180b4490fdc706958f45
kshetritej <tejghartikshetri@gmail.com>
2026-03-08 12:08:18 +0545

    update: whatsapp phone number

--- components/atoms/whatsapp-phone-button.tsx
+++ components/atoms/whatsapp-phone-button.tsx
@@ -1,15 +1,22 @@
+import { siteConfig } from "@/lib/siteConfig";
 import { LucidePhone } from "lucide-react";
+import Link from "next/link";
 
 export function CallNow() {
   return (
-    <div className="items-center gap-2 hidden md:flex">
+    <Link
+      href={`https://api.whatsapp.com/send/?phone=${siteConfig.whatsAppNumber}`}
+      className="items-center gap-2 hidden md:flex"
+    >
       <div className="rounded-full aspect-square bg-sky-900 p-2">
         <LucidePhone className="text-white" />
       </div>
       <div className="text-right">
-        <p className="font-bold text-sm md:text-lg">+977 9841328947</p>
+        <p className="font-bold text-sm md:text-lg">
+          {siteConfig.whatsAppNumber}
+        </p>
         <p className="text-xs md:text-sm">Direct Call or WhatsApp 24/7</p>
       </div>
-    </div>
+    </Link>
   );
 }
--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -2,7 +2,6 @@ import LogoComponent from "@/components/atoms/logo";
 import { MegaMenu } from "./mega-menu";
 import { MobileMenu } from "./mobile-menu";
 import { CallNow } from "../atoms/whatsapp-phone-button";
-import { Mail, Phone } from "lucide-react";
 
 export default async function Navbar() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/menu`);
@@ -12,17 +11,6 @@ export default async function Navbar() {
   const menuData = data?.data?.items || [];
 
   return (
-    // <div
-    //   id="site-navbar"
-    //   className="sticky top-0 shadow-md p-2 z-999 bg-background"
-    // >
-    // <div className="max-w-6xl mx-auto p-2 flex justify-between gap-2 pb-4">
-    //   <LogoComponent dark />
-    //   <CallNow />
-    // </div>
-
-    // <div className="h-0.5 w-full bg-accent" />
-
     <div
       id="site-navbar"
       className="min-w-screen! mx-auto sticky top-0 bg-background shadow-b-sm flex flex-col gap-1 justify-between p-2  z-999"
