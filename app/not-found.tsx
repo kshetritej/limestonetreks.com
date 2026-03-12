2dbb1dd8da940f70d4b178238c4a17cdc4cdde71
kshetritej <tejghartikshetri@gmail.com>
2026-03-12 17:36:39 +0545

    chore: use Image tag in not found tag

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,19 @@
 import { Button } from "@/components/ui/button";
 import svg from "../public/not-found-illustration.svg";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-12 items-center justify-center min-h-[60vh]">
       <p className="font-bold text-3xl">Page not Found!</p>
-      <img className="w-60 " src={svg.src} />
+      <Image
+        className="w-60"
+        alt="not found image"
+        height={200}
+        width={200}
+        src={svg.src}
+      />
       <Link href={"/"}>
         <Button>Go Back Home</Button>
       </Link>
